e19433123c97a7bb902e20a77b6c98e9625a3db0
mainpedrocosta <1221790@isep.ipp.pt>
2024-04-13 16:20:55 +0100

    US003: Started the implementation of class Adress

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Address.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Address.java
@@ -1,4 +1,59 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
 public class Address {
+    private String street;
+    private int streetNumber;
+    private String postalCode;  //Verificar tipo de dado do postal code
+    private String city;
+    private String district;
+
+    public Address(String street, int streetNumber, String postalCode, String city, String district) {
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.district = district;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+
 }
