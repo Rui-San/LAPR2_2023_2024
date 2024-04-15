3bdfeb1fbd9e7e5c930dea7de0d2faa1029086e9
1230608franciscotrocado <1230608@isep.ipp.pt>
2024-04-15 16:22:02 +0100

    US010 - SSD Created classes Job & JobRepository

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package pt.ipp.isep.dei.esoft.project.domain.repository;
+
+import pt.ipp.isep.dei.esoft.project.domain.Job;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JobRepository {
+
+
+
+}
