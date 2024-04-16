fec579644c7d0b9a569c9e26bed118babb42db6d
Pedro <1221790@isep.ipp.pt>
2024-04-16 12:40:49 +0100

    RouteImporter class update, fix something on the algorithm

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/matdisc/RouteImporter.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/matdisc/RouteImporter.java
@@ -2,28 +2,50 @@ package pt.ipp.isep.dei.esoft.project.domain.matdisc;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 
 public class RouteImporter {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("What is the path of the File?");
+        String fileName = ler.nextLine();
+
+        try {
+            RouteImporter routeImporter = new RouteImporter(fileName);
+            List<Route> routesList = routeImporter.importRoutes();
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found, please type a valid path");
+        }
+    }
 
     private File fileCsv;
 
-    private static final String DIVISOR_CSV = ",";
+    private static final String DIVISOR_CSV = ";";
 
-    public RouteImporter(File fileCsv) {
-        this.fileCsv = fileCsv;
+    public RouteImporter(String pathName) {
+        File csv = new File(pathName);
+        this.fileCsv = csv;
     }
 
-    public ArrayList<Route> importRoutes() throws FileNotFoundException {
+    public File getFileCsv() {
+        return fileCsv;
+    }
 
-        ArrayList<Route> routesList = new ArrayList<>();
+    public List<Route> importRoutes() throws FileNotFoundException {
+
+        List<Route> routesList;
+        routesList = new ArrayList<>();
+
         String linha;
         String[] dados;
-        Scanner in = new Scanner(fileCsv);
+
+        Scanner in = new Scanner(getFileCsv());
 
         while (in.hasNextLine()) {
-            linha = in.nextLine();
+            linha = in.nextLine().trim();
             dados = linha.split(DIVISOR_CSV);
 
             int waterPointX = Integer.parseInt(dados[0]);
