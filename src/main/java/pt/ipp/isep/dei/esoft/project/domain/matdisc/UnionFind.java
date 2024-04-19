79e72aac9a7f75e7aca86f70ff216438b9e80336
Pedro <1221790@isep.ipp.pt>
2024-04-19 09:05:45 +0100

    MATDISC US - Class UnionFind: - Added JavaDoc comments for the class and for the methods.

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/matdisc/UnionFind.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/matdisc/UnionFind.java
@@ -3,6 +3,13 @@ package pt.ipp.isep.dei.esoft.project.domain.matdisc;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The UnionFind class is responsible for, when calling the method union with 2 diferent vertex,
+ * it checks if they belong to the same set. If they do, it means connecting them would create a
+ * cycle, so nothing is done. If they are in different sets, the union method joins them, vertex2
+ * going to the same set as vertex1.
+ */
+
 public class UnionFind {
 
     private final List<String> parent;
@@ -11,6 +18,16 @@ public class UnionFind {
         parent = new ArrayList<>();
     }
 
+    /**
+     * Used to unite 2 diferent sets, each set with one vertex
+     * Receiving 2 diferent verifies if they belong to the same set.
+     * If they belong to the same set, nothing is done
+     * The union is done by changing the parent of the vertex2, pointing it to the vertex1 instead
+     *
+     * @param vertex1
+     * @param vertex2
+     * @return
+     */
     public boolean union(String vertex1, String vertex2) {
         int root1 = findRoot(vertex1);
         int root2 = findRoot(vertex2);
@@ -22,6 +39,13 @@ public class UnionFind {
         return false; // União criaria um ciclo
     }
 
+    /**
+     * this is used to find the root that the vertex belongs
+     * Searching algorithm that navigates through the parents of the vertices until it finds the root.
+     *
+     * @param vertex
+     * @return the index of list parent that corresponds to the root of that set
+     */
     public int findRoot(String vertex) {
         if (!parent.contains(vertex)) {
             parent.add(vertex); // Adiciona o vértice ao conjunto
