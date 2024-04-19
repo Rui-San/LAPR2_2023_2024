25fe33f7833eb433350d6ac94add2e3e8b96de3f
1230608franciscotrocado <1230608@isep.ipp.pt>
2024-04-19 15:29:19 +0100

    US002 - Finished classes Job & JobRepository

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Job.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Job.java
@@ -1,6 +1,51 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
-public class Job {
+import java.util.regex.Pattern;
 
+public class Job {
+    private String jobName;
+
+    private enum JobValidationResults {
+        EMPTYNULL, VALID, CONTAINS_SPECIAL_CHARACTERS,
+    }
+
+    public Job(String jobName) {
+        setJobName(jobName);
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        Job.JobValidationResults jobValidationResults = validateJob(jobName);
+        switch (jobValidationResults) {
+            case EMPTYNULL:
+                throw new IllegalArgumentException("Job name must not be empty");
+            case CONTAINS_SPECIAL_CHARACTERS:
+                throw new IllegalArgumentException("Job name must not contain special characters or numbers");
+            case VALID:
+                this.jobName = jobName;
+        }
+    }
+
+    @Override
+    public Job clone() {
+        return new Job(this.jobName);
+    }
+
+    private Job.JobValidationResults validateJob(String jobName) {
+        if (jobName == null || jobName.trim().isEmpty()) {
+            return Job.JobValidationResults.EMPTYNULL;
+        }
+
+        Pattern namePattern = Pattern.compile("[a-zA-Z\\\\s-]+");
+
+        if (namePattern.matcher(jobName).matches()) {
+            return Job.JobValidationResults.VALID;
+        } else {
+            return Job.JobValidationResults.CONTAINS_SPECIAL_CHARACTERS;
+        }
+    }
 
 }
