f2c1c677ba21989a921e43ca386515ff411e3941
Pedro <1221790@isep.ipp.pt>
2024-04-26 10:27:39 +0100

    Class DevelopmentTeamUI: Updated team members

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DevelopmentTeamUI.java
@@ -0,0 +1,19 @@
+package pt.ipp.isep.dei.esoft.project.ui.console;
+
+public class DevelopmentTeamUI implements Runnable {
+
+    public DevelopmentTeamUI() {
+
+    }
+    @Override
+    public void run() {
+        System.out.println("\n");
+        System.out.println("--- DEVELOPMENT TEAM -------------------");
+        System.out.println("  Pedro Costa - 1221790@isep.ipp.pt");
+        System.out.println("  Rui Santiago - 1221402@isep.ipp.pt");
+        System.out.println("  Diógenes Jesus - 1210879@isep.ipp.pt");
+        System.out.println("  Francisco Trocado - 1230608@isep.ipp.pt");
+        System.out.println("  Alexandre Moura - 1231193@isep.ipp.pt");
+        System.out.println("\n");
+    }
+}
