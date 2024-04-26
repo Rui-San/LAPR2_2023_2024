3f41c9d9929a7f44a36e39bff689d28468b7f6b1
Rui Santiago <1221402@isep.ipp.pt>
2024-04-26 13:50:02 +0100

    RegisterCheckupUI: Added - method to validate typed date - methods that request typed data - method that lists and lets the user choose which vehicle to add checkup to - Main method for testing purposes

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterCheckupUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterCheckupUI.java
@@ -1,8 +1,139 @@
 package pt.ipp.isep.dei.esoft.project.ui.console;
 
+import jdk.jshell.ImportSnippet;
+import pt.ipp.isep.dei.esoft.project.application.controller.RegisterCheckupController;
+import pt.ipp.isep.dei.esoft.project.domain.Date;
+import pt.ipp.isep.dei.esoft.project.domain.Vehicle;
+import pt.ipp.isep.dei.esoft.project.domain.VehicleCheckup;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class RegisterCheckupUI implements Runnable {
-    @Override
+
+    public static void main(String[] args) {
+        RegisterCheckupUI ui = new RegisterCheckupUI();
+        ui.run();
+    }
+
+    private final RegisterCheckupController controller;
+    private Vehicle vehicle;
+    private String checkupDate;
+    private int checkupKm;
+
+    public RegisterCheckupUI() {
+        controller = new RegisterCheckupController();
+    }
+
+    private RegisterCheckupController getController() {
+        return controller;
+    }
+
     public void run() {
+        System.out.println("\n ----------REGISTER VEHICLE CHECKUP ----------------\n");
+
+        vehicle = displayAndSelectVehicle();
+
+        requestData();
+
+        submitData();
+
+        ArrayList<VehicleCheckup> checkupList = getController().getCheckupRepository().getVehicleCheckupList();
+
+        System.out.println("\nCheckup list:");
+        for (VehicleCheckup checkup : checkupList) {
+            System.out.println(checkup.toString());
+        }
+    }
+
+    private void submitData() {
+        Optional<VehicleCheckup> checkup = getController().registerVehicleCheckup(vehicle, checkupDate, checkupKm);
+
+        if (checkup.isPresent()) {
+            System.out.println("\nCheckup successfully created!");
+        } else {
+            System.out.println("\nCheckup not created!");
+        }
+    }
+
+    private void requestData() {
+
+        //Request the checkup date
+        checkupDate = requestCheckupDate();
+
+        //Request the kilometrage at checkup
+        checkupKm = requestCheckupKm();
 
     }
+
+    private String requestCheckupDate() {
+        String date;
+        Scanner read = new Scanner(System.in);
+        do {
+            System.out.printf("\nCheckup date (DD/MM/YYYY): ");
+            date = read.nextLine();
+        }while (!validDate(date));
+        return date;
+    }
+
+    boolean validDate(String date){
+        LocalDate today = LocalDate.now();
+        String[] dateSplit = date.split("/");
+        if(dateSplit.length != 3){ return false;}
+        if(Integer.parseInt(dateSplit[0]) < 0 || Integer.parseInt(dateSplit[0]) > 31){ return false;}
+        if(Integer.parseInt(dateSplit[1]) < 0 || Integer.parseInt(dateSplit[0]) > 12){ return false;}
+        if(Integer.parseInt(dateSplit[2]) < 0 || Integer.parseInt(dateSplit[1]) > today.getYear()){ return false;}
+        return true;
+    }
+
+    private int requestCheckupKm() {
+        int checkupKm = -1;
+        Scanner read = new Scanner(System.in);
+        do {
+            System.out.printf("\nCheckup Km: ");
+            checkupKm = read.nextInt();
+        }while (checkupKm < 0 || checkupKm > vehicle.getCurrentKm());
+        return checkupKm;
+    }
+
+    private Vehicle displayAndSelectVehicle() {
+
+        //this is the correct way of doing it. This list in unmodifiable
+        //List<Vehicle> vehicleList = controller.getVehicles();
+
+        List<Vehicle> vehicleList = new ArrayList<>();
+
+        //This is a mockup
+        Vehicle vehicle1 = new Vehicle("11-11-11", "Car", "Gasoline", "type1", 1000, 1000, 1000, new Date(1,12,2003), new Date(2, 3, 2000), 1000);
+        Vehicle vehicle2 = new Vehicle("22-22-22", "Car", "Gasoline", "type2", 2000, 2000, 2000, new Date(1,12,2003), new Date(2, 3, 2000), 2000);
+        vehicleList.add(vehicle1);
+        vehicleList.add(vehicle2);
+
+        int numberOfVehicles = vehicleList.size();
+        int answer = -1;
+
+        Scanner input = new Scanner(System.in);
+
+        while (answer < 1 || answer > numberOfVehicles) {
+            displayVehicleOptions(vehicleList);
+            System.out.print("Vehicle to register checkup: ");
+            answer = input.nextInt();
+        }
+
+        Vehicle vehicle = vehicleList.get(answer - 1);
+        return vehicle;
+    }
+
+    private void displayVehicleOptions(List<Vehicle> vehicleList) {
+        //display the task categories as a menu with number options to select
+        int i = 1;
+        for (Vehicle vehicle : vehicleList) {
+            System.out.println("  " + i + " - " + vehicle.getPlateId());
+            i++;
+        }
+    }
+
 }
