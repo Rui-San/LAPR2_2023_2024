e0bd8f98f4ccdcdbf46c53f65e31e12f0ed2539b
Diogenes-c <1210879@isep.ipp.pt>
2024-04-29 15:37:51 +0100

    RegisterSkillController: created

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterSkillController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterSkillController.java
@@ -1,6 +1,20 @@
-package pt.ipp.isep.dei.esoft.project.application;
+package pt.ipp.isep.dei.esoft.project.application.controller;
+
+import pt.ipp.isep.dei.esoft.project.domain.Skill;
+import pt.ipp.isep.dei.esoft.project.repository.SkillRepository;
+
+import java.util.Optional;
 
 public class RegisterSkillController {
+    private final SkillRepository skillRepository;
 
+    public RegisterSkillController() {
+        skillRepository = new SkillRepository();
+    }
 
+    public boolean registerSkill(String skillName) {
+        Skill newSkill = new Skill(skillName);
+        Optional<Skill> addedSkill = skillRepository.add(newSkill);
+        return addedSkill.isPresent();
+    }
 }
