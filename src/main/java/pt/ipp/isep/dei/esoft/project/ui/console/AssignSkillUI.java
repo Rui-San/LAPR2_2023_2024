2c4c31f31ddfd2ac54d10b4b6371e78cb15ceaae
Diogenes-c <1210879@isep.ipp.pt>
2024-05-01 16:44:30 +0100

    AssignSkillUI: methods created

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/AssignSkillUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/AssignSkillUI.java
@@ -1,8 +1,72 @@
 package pt.ipp.isep.dei.esoft.project.ui.console;
 
-public class AssignSkillUI implements Runnable{
+import pt.ipp.isep.dei.esoft.project.repository.CollaboratorRepository;
+import pt.ipp.isep.dei.esoft.project.repository.SkillRepository;
+import pt.ipp.isep.dei.esoft.project.application.controller.AssignSkillController;
+import pt.ipp.isep.dei.esoft.project.domain.Collaborator;
+import pt.ipp.isep.dei.esoft.project.domain.Skill;
+import java.util.ArrayList;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Scanner;
+
+public class AssignSkillUI implements Runnable {
+    private final AssignSkillController controller;
+    private Collaborator selectedCollaborator;
+    private List<Skill> selectedSkills;
+
+    public AssignSkillUI() {
+        controller = new AssignSkillController();
+    }
+
+    private void requestData() {
+        Scanner scanner = new Scanner(System.in);
+
+        CollaboratorRepository collaboratorRepository = new CollaboratorRepository();
+        List<Collaborator> collaborators = collaboratorRepository.getCollaboratorList();
+        // Display the collaborators and ask the user to select one
+        System.out.println("Select the collaborator:");
+        for (int i = 0; i < collaborators.size(); i++) {
+            System.out.println(i + 1 + ". " + collaborators.get(i).getName());
+        }
+        int collaboratorIndex = scanner.nextInt() - 1;
+        selectedCollaborator = collaborators.get(collaboratorIndex);
+
+        SkillRepository skillRepository = new SkillRepository();
+        List<Skill> skills = skillRepository.getSkillList();
+        // Display the skills and ask the user to select one or more
+        System.out.println("Select the skills:");
+        for (int i = 0; i < skills.size(); i++) {
+            System.out.println(i + 1 + ". " + skills.get(i).getName());
+        }
+        selectedSkills = new ArrayList<>();
+        while (true){
+            int skillIndex = scanner.nextInt() -1;
+            if (skillIndex < 0 || skillIndex >= skills.size()) {
+                break;
+            }
+            selectedSkills.add(skills.get(skillIndex));
+        }
+    }
+
+    private void submitData() {
+        boolean isAssigned = controller.assignSkillsToCollaborator(selectedCollaborator, selectedSkills);
+
+        if (isAssigned) {
+            System.out.println("\nSkills assigned!");
+        } else {
+            System.out.println("\nAssignment failed!");
+        }
+    }
+
     @Override
     public void run() {
+        requestData();
+        submitData();
+    }
 
+    public static void main(String[] args) {
+        AssignSkillUI assignSkillUI = new AssignSkillUI();
+        assignSkillUI.run();
     }
 }
