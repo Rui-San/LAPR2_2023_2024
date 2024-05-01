a1bed3a3b06ae7b609a0c15780aa151c43dc217c
Rui Santiago <1221402@isep.ipp.pt>
2024-05-01 17:43:19 +0100

    VehicleCheckup: Added JavaDoc for all methods and validation for the checkupKms

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/VehicleCheckup.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/VehicleCheckup.java
@@ -2,30 +2,94 @@ package pt.ipp.isep.dei.esoft.project.domain;
 
 import pt.ipp.isep.dei.esoft.project._templateFiles.domain.TaskCategory;
 
+/**
+ * Represents a Vehicle Checkup, through its Vehicle, checkup date and checkup kilometers.
+ */
 public class VehicleCheckup {
 
+    /**
+     *  The vehicle selected to register the checkup.
+     */
     private Vehicle vehicle;
+
+    /**
+     * The date of the checkup.
+     */
     private Date checkupDate;
+
+    /**
+     * The kilometers of the vehicle at the time of the checkup.
+     */
     private int checkupKms;
 
+    /**
+     * Gets the vehicle of a checkup.
+     * @return
+     */
     public Vehicle getVehicle() { return vehicle; }
 
+    /**
+     * Gets the date of a checkup.
+     * @return
+     */
     public Date getCheckupDate() { return checkupDate; }
 
+    /**
+     * Gets the kilometers of a checkup.
+     * @return
+     */
     public int getCheckupKms() { return checkupKms; }
 
+    /**
+     * Sets the vehicle of a checkup.
+     * @param vehicle the vehicle that was checked
+     */
     public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
 
+    /**
+     * Sets the date of a checkup.
+     * @param checkupDate the date of the checkup
+     */
     public void setCheckupDate(Date checkupDate) { this.checkupDate = checkupDate; }
 
-    public void setCheckupKms(int checkupKms) { this.checkupKms = checkupKms; }
-
-    public VehicleCheckup(Vehicle vehicle, Date checkupDate, int checkupKms) {
-        this.vehicle = vehicle;
-        this.checkupDate = checkupDate;
+    /**
+     * Sets the kilometers of a checkup.
+     * @param checkupKms the kilometers at the time of the checkup
+     */
+    public void setCheckupKms(int checkupKms) {
+        validateCheckupKm(this.vehicle, checkupKms);
         this.checkupKms = checkupKms;
     }
 
+    /**
+     * Instantiates a new Vehicle Checkup.
+     * @param vehicle the vehicle
+     * @param checkupDate the checkup date
+     * @param checkupKms the checkup kilometers
+     */
+    public VehicleCheckup(Vehicle vehicle, Date checkupDate, int checkupKms) {
+        setVehicle(vehicle);
+        setCheckupDate(checkupDate);
+        setCheckupKms(checkupKms);
+    }
+
+    /**
+     * Validates the checkup kilometers.
+     * @param vehicle the vehicle
+     * @param checkupKms the checkup kilometers
+     */
+    private void validateCheckupKm(Vehicle vehicle, int checkupKms) {
+        if (checkupKms < 0) {
+            throw new IllegalArgumentException("Checkup kilometers must be a positive number.");
+        }else if(checkupKms > vehicle.getCurrentKm()){
+            throw new IllegalArgumentException("Checkup kilometers must be less than the current kilometers of the vehicle.");
+        }
+    }
+
+    /**
+     * Returns the textual representation of the Vehicle Checkup. In this case, showing the vehicle, checkup date and checkup kilometers.
+     * @return the textual representation of the Vehicle Checkup
+     */
     @Override
     public String toString() {
         return "VehicleCheckup{" +
@@ -35,6 +99,10 @@ public class VehicleCheckup {
                 '}';
     }
 
+    /**
+     * Creates a deep copy of the checkup
+     * @return a deep copy of the checkup
+     */
     @Override
     public VehicleCheckup clone() {
         return new VehicleCheckup(this.vehicle, this.checkupDate, this.checkupKms);
