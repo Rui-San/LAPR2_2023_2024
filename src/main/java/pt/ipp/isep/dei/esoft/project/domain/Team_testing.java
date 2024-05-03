7da619ff1ceb3f1ae3053cea39d7b09fa20e3d88
Pedro <1221790@isep.ipp.pt>
2024-05-03 11:15:20 +0100

    US05 - team generate:
    implemented a new algorithm to create all possibles teams for each team size.

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Team_testing.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Team_testing.java
@@ -8,12 +8,18 @@ import java.util.List;
 public class Team_testing {
     private List<Collaborator> members;
 
+    public Team_testing(List<Collaborator> members) {
+        this.members = members;
+    }
+
     public Team_testing() {
 
     }
     public List<Collaborator> getMembers() {
         return new ArrayList<>(members);
     }
+
+
 
     public void addMember(Collaborator collaborator) {
         members.add(collaborator);
