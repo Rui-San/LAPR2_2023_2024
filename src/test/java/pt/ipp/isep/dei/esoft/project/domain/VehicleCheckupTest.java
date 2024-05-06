0e11a91f6d2b2453836e5a4ba1516efd08c8fbb0
Rui Santiago <1221402@isep.ipp.pt>
2024-05-06 16:26:22 +0100

    VehicleCheckupTest: added test methods

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/VehicleCheckupTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/VehicleCheckupTest.java
@@ -43,5 +43,44 @@ class VehicleCheckupTest {
         assertEquals(vehicle, checkup.getVehicle());
     }
 
+    @Test
+    void testGetCheckupkms(){
+        Vehicle vehicle = new Vehicle("AA-00-00", "Vehicle Brand", "Vehicle Model", "1", 10, 10, 1000, new Date("10/12/2003"), new Date("10/12/2012"), 100 );
+        VehicleCheckup checkup = new VehicleCheckup(vehicle, new Date("10/12/2012"), 100);
+        assertEquals(100, checkup.getCheckupKms());
+    }
+
+    @Test
+    void testGetCheckupDate(){
+        Vehicle vehicle = new Vehicle("AA-00-00", "Vehicle Brand", "Vehicle Model", "1", 10, 10, 1000, new Date("10/12/2003"), new Date("10/12/2012"), 100 );
+        VehicleCheckup checkup = new VehicleCheckup(vehicle, new Date("10/12/2012"), 100);
+        assertEquals(new Date("10/12/2012"), checkup.getCheckupDate());
+    }
+
+    @Test
+    void testSetVehicle(){
+        Vehicle vehicle = new Vehicle("AA-00-00", "Vehicle Brand", "Vehicle Model", "1", 10, 10, 1000, new Date("10/12/2003"), new Date("10/12/2012"), 100 );
+        Vehicle vehicle2 = new Vehicle("BB-00-00", "Vehicle Brand", "Vehicle Model", "1", 10, 10, 1000, new Date("10/12/2003"), new Date("10/12/2012"), 100 );
+        VehicleCheckup checkup = new VehicleCheckup(vehicle, new Date("10/12/2012"), 100);
+        checkup.setVehicle(vehicle2);
+        assertEquals(vehicle2, checkup.getVehicle());
+    }
+
+    @Test
+    void testSetCheckupkms(){
+        Vehicle vehicle = new Vehicle("AA-00-00", "Vehicle Brand", "Vehicle Model", "1", 10, 10, 1000, new Date("10/12/2003"), new Date("10/12/2012"), 100 );
+        VehicleCheckup checkup = new VehicleCheckup(vehicle, new Date("10/12/2012"), 100);
+        checkup.setCheckupKms(200);
+        assertEquals(200, checkup.getCheckupKms());
+    }
+
+    @Test
+    void testSetCheckupDate(){
+        Vehicle vehicle = new Vehicle("AA-00-00", "Vehicle Brand", "Vehicle Model", "1", 10, 10, 1000, new Date("10/12/2003"), new Date("10/12/2012"), 100 );
+        VehicleCheckup checkup = new VehicleCheckup(vehicle, new Date("10/12/2012"), 100);
+        checkup.setCheckupDate(new Date("10/12/2013"));
+        assertEquals(new Date("10/12/2013"), checkup.getCheckupDate());
+    }
+
 
 }
