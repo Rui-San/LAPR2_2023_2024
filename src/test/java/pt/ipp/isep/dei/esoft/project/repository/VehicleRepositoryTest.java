2546d85d0bd1c68132a2ba9c9ea85f73be839abb
Rui Santiago <1221402@isep.ipp.pt>
2024-05-12 00:28:03 +0100

    Added tests for VehicleRepository and CheckupRepository

---
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/VehicleRepositoryTest.java
@@ -0,0 +1,113 @@
+package pt.ipp.isep.dei.esoft.project.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.esoft.project.domain.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class VehicleRepositoryTest {
+
+    private VehicleRepository vehicleRepository;
+    private Vehicle vehicle;
+    private VehicleCheckup vehicleCheckup;
+    private CheckupRepository checkupRepository;
+
+    @BeforeEach
+    public void createData() {
+        vehicleRepository = new VehicleRepository();
+        vehicle = new Vehicle(
+                "AA-00-AA",
+                "Toyota",
+                "Corolla",
+                "Sedan",
+                1500,
+                2000,
+                10000,
+                "01/01/2023",
+                "01/05/2024",
+                5000
+        );
+        vehicleRepository.add(vehicle);
+        checkupRepository = new CheckupRepository();
+        vehicleCheckup = new VehicleCheckup(vehicle, new Date("01/01/2023"), 1500);
+        checkupRepository.add(vehicleCheckup);
+    }
+
+    @Test
+    public void testAddVehicle() {
+
+        Optional<Vehicle> addedVehicle = vehicleRepository.add(vehicle);
+
+        assertTrue(addedVehicle.isPresent());
+    }
+
+    @Test
+    void testGetVehicleList() {
+
+        List<Vehicle> vehicleList = vehicleRepository.getVehicleList();
+
+        assertNotNull(vehicleList);
+        assertEquals(1, vehicleList.size());
+    }
+
+    @Test
+    void testGetVehicleByPlateID() {
+
+        Vehicle retrievedVehicle = vehicleRepository.getVehicleByPlateId(vehicle.getPlateId());
+
+        assertEquals(vehicle.getPlateId(), retrievedVehicle.getPlateId());
+    }
+
+    @Test
+    void testGetVehiclesNeedingCheckup(){
+
+        List<VehicleNeedingCheckup> vehiclesNeedingCheckup = vehicleRepository.getVehiclesNeedingCheckup(checkupRepository.getVehicleCheckups());
+
+        assertNotNull(vehiclesNeedingCheckup);
+        assertEquals(1, vehiclesNeedingCheckup.size());
+    }
+
+    @Test
+    void testCreateVehicle() {
+
+        Optional<Vehicle> addedVehicle = vehicleRepository.createVehicle(
+                "AA-00-AA",
+                "Toyota",
+                "Corolla",
+                "Sedan",
+                1500,
+                2000,
+                10000,
+                "01/01/2023",
+                "01/05/2024",
+                5000
+        );
+
+        assertTrue(addedVehicle.isPresent());
+    }
+
+    @Test
+    void testGetLastCheckupKm() {
+
+        int lastCheckupKm = vehicleRepository.getLastCheckupKm(vehicle, checkupRepository.getVehicleCheckups());
+
+        assertEquals(1500, lastCheckupKm);
+    }
+
+    @Test
+    void testNeedsCheckup() {
+
+        int lastCheckupKm = vehicleRepository.getLastCheckupKm(vehicle, checkupRepository.getVehicleCheckups());
+        int optimalNextCheckupKms = lastCheckupKm + vehicle.getCheckupFrequencyKms();
+        boolean needsCheckup = vehicleRepository.needsCheckup(vehicle, lastCheckupKm, optimalNextCheckupKms);
+
+        assertTrue(needsCheckup);
+
+    }
+
+}
