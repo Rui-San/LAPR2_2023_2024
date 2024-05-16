6ea6a9e7794b8fdeb6d2c819708e289d340882dc
Rui Santiago <1221402@isep.ipp.pt>
2024-05-17 00:28:52 +0100

    added RegisterCollaboratorUI (JavaFX) without any methods

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/RegisterCollaboratorUI.java
@@ -0,0 +1,15 @@
+package pt.ipp.isep.dei.esoft.project.ui.gui;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import java.awt.*;
+
+public class RegisterCollaboratorUI {
+
+    @FXML
+    private TextArea txtName;
+
+    @FXML
+    private DatePicker dpBirthDate;
+
+
+}
