c63da1f64dc6e620197a4b48b87461caff1cae4e
Pedro <1221790@isep.ipp.pt>
2024-05-17 12:51:26 +0100

    Created class Status enumerate for tasks

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Task.java
@@ -0,0 +1,16 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+import pt.ipp.isep.dei.esoft.project.tools.Status;
+
+public class Task {
+
+    private String title;
+    private String description;
+    private Status status;
+
+
+
+
+
+
+}
