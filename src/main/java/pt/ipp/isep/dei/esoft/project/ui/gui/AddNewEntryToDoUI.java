677d3f7eb7cc3c931eb3aceaf7964163073e85e2
Pedro <1221790@isep.ipp.pt>
2024-05-22 09:44:26 +0100

    NewEntryToDo fxml created

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/AddNewEntryToDoUI.java
@@ -0,0 +1,20 @@
+package pt.ipp.isep.dei.esoft.project.ui.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddNewEntryToDoUI implements Initializable {
+    public void btnSubmit(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void btnClear(ActionEvent actionEvent) {
+    }
+}
